ec6b8df4f0f3d373a51dc22b2eec213991d6fe5d
romeoliveirasantos <rjob.oliveira@gmail.com>
2025-03-07 16:54:53 -0300

    Invocação direta, call, apply e operador new

--- javascript/FirstClassFunctions_HighOrderFunctions_FunctionDeclaration_FunctionExpression/invocacaoDiretaCallApplyOperadorNew.js
+++ javascript/FirstClassFunctions_HighOrderFunctions_FunctionDeclaration_FunctionExpression/invocacaoDiretaCallApplyOperadorNew.js
@@ -1,1 +1,44 @@
 //Invocação direta, call, aplly e operador new
+
+//invocação direta
+function teste (){
+  console.log('invocação direta, declaração explicita')
+}
+//invocação direta 
+// teste()
+
+
+//call e apply para invocar a função
+
+
+//metodo apply -> invocação indireta
+const pessoa = {
+  nome: 'Romario',
+  idade : 30
+}
+
+function gritar(prefixo, prefixo2){
+  console.log(prefixo, this.nome)
+}
+
+//execução direta
+gritar('Oláaaa')
+//primeiro passamos o contexto e depois os atributos da função
+gritar.apply(pessoa, ['Olaaa'])
+
+//metodo call-> invocação indireta
+//o resultado é igual, com a diferença que não recebe um array e sim o argumento direto
+gritar.call(pessoa, 'Olaaaaa')
+
+//operador new
+//passa o contexto para a função e retorna um objeto pronto
+const pessoa2 = {
+  nome: 'Romario',
+  idade: 30,
+  falar: function(frase, nome){
+    this.nome = nome;
+    console.log(`${frase}, ${this.nome}`)
+  }
+}
+//instanciando com o new
+new pessoa2.falar('Olá', 'Romário')
