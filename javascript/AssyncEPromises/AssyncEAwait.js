cd3d083eb61061546a470f92aa9f7794a2ecca11
romeoliveirasantos <rjob.oliveira@gmail.com>
2025-03-09 18:58:40 -0300

    Requisição utilizando função async await

--- javascript/AssyncEPromises/AssyncEAwait.js
+++ javascript/AssyncEPromises/AssyncEAwait.js
@@ -8,14 +8,48 @@ const filePath = path.resolve(__dirname, 'tarefas.csv')
 
 //async await
 //para usar async await precisamos de uma função async
-async function buscarArquivo(){
-  try{
-    const arquivo = await fs.promises.readFile(filePath)
-    const textoDoArquivo = arquivo.toString('utf8')
-    console.log(textoDoArquivo)
-  }catch(error){
-    console.error('Erro: ', error.message)
-  }
+// async function buscarArquivo(){
+//   try{
+//     const arquivo = await fs.promises.readFile(filePath)
+//     const textoDoArquivo = arquivo.toString('utf8')
+//     console.log(textoDoArquivo)
+//   }catch(error){
+//     console.error('Erro: ', error.message)
+//   }
 
+// }
+// buscarArquivo()
+
+
+//exemplo de async await function que faz uma requisição get na API 
+async function getCharacter(name){
+  const url = `https://api.tibiadata.com/v4/character/${name}`;
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error(`Erro na API: Status - ${response.status} ${response.statusText}`);
+    }
+
+    // 🔹 Captura o JSON da resposta
+    const data = await response.json();
+
+    // 🔹 Verifica se 'characters' e 'character' existem antes de acessar
+        if (!data.character || !data.character.character) {
+            throw new Error("Personagem não encontrado na resposta da API.");
+        }
+        // 🔹 Desestruturação para acessar diretamente o nome
+        const { character } = data.character;
+        const { name: characterName, level: characterLevel, vocation } = character;
+        //name e level estão sendo renomeadas
+    console.log(`Nome: ${characterName}`);
+    console.log(`Level: ${characterLevel}`);
+    console.log(`Vocação: ${vocation}`);
+
+    return character; // Retorna o nome do personagem
+} catch (e) {
+    console.error("Erro ao buscar personagem:", e.message);
 }
-buscarArquivo()
+}
+
+getCharacter('roshoc')
